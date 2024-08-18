e468cc5285c9e14338cd99a8e1273a0892aeeb54
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-08-18 13:25:05 +0000

    Initial commit

---
+++ src/pages/Início.c1dmp.js
@@ -0,0 +1,29 @@
+// Guia de API: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	// Escreva seu código JavaScript aqui usando o API de framework do Velo
+
+	// Exiba hello world:
+	// console.log("Hello world!");
+
+	// Chame funções em elementos da página, por exemplo:
+	// $w("#button1").label = "Clique aqui!";
+
+	// Clique em "Executar" ou visualize seu site para executar seu código
+
+});
+
+
+
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function box5_click(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
+
